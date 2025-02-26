0dfb65c687b39fc679589b467275bd89c38c148f
Nadir Hossain <nadirhossain336@gmail.com>
2025-02-26 12:06:09 +0600

    fix: :bugs: fix the the video related issue

--- src/app.ts
+++ src/app.ts
@@ -21,8 +21,8 @@ app.use(
 
 //body parser
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: "1000mb"}));
+app.use(express.urlencoded({ extended: true, limit: "1000mb" }));
 
 //file retrieve
 app.use(express.static('uploads'));
