4692e877df943beabb39e52b3a0470bf0f38abb8
Nadir Hossain <nadirhossain336@gmail.com>
2025-03-17 09:48:28 +0600

    refactor: :recycle: refactor cors section

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(
 //body parser
 app.use(cors({
     origin: ['https://api.learnova.info', "https://mahmud.binarybards.online" , "https://learnova.info"],
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
